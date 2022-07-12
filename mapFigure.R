81eab8bb76e09fe6667af374d899b3761cab008a
makosnik <32238202+makosnik@users.noreply.github.com>
2022-07-12 14:18:52 +1000

    Update mapFigure.R
    added ref.

--- mapFigure.R
+++ mapFigure.R
@@ -16,6 +16,15 @@
 #	
 ################################################################################
 
+################################################################################
+## Yvette Bauder, Briony Mamo, Glenn A. Brock, Matthew A. Kosnik. 2022 (In revision). 
+## One Tree Reef Foraminifera: a relic of the pre-colonial Great Barrier Reef
+## Conservation Palaeobiology of Marine Ecosystems: Concepts and Applications
+## Geological Society of London Special Publication.
+##
+## Contact: Matthew Kosnik, Matthew.Kosnik@mq.edu.au or mkosnik@alumni.uchicago.edu
+################################################################################
+
 ## USE THE SAME PLOT PAR ACROSS ALL PLOTS
 source('plotPreferences.R')
 fontFamily <- 'Helvetica'	
